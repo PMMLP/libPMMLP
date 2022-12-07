34235173d43a73943a04ef66bab8b4d89f8998b5
Matze997 <matthias.kuegele@gmail.com>
2022-12-07 23:21:49 +0100

    ConfigParser: Users should be able to force their result

--- src/pmmlp/config/ConfigParser.php
+++ src/pmmlp/config/ConfigParser.php
@@ -8,14 +8,15 @@ use pmmlp\item\StringToItemParser;
 use pocketmine\crafting\CraftingRecipe;
 use pocketmine\crafting\ShapedRecipe;
 use pocketmine\crafting\ShapelessRecipe;
+use pocketmine\item\Item;
 
 class ConfigParser {
-    public static function getCraftingRecipe(array $recipe): CraftingRecipe {
+    public static function getCraftingRecipe(array $recipe, ?Item $result = null): CraftingRecipe {
         $ingredients = [];
         foreach($recipe["items"] as $key => $stringItem) {
             $ingredients[$key] = StringToItemParser::parse($stringItem);
         }
-        $result = [StringToItemParser::parse($recipe["result"])];
+        $result ??= [StringToItemParser::parse($recipe["result"])];
         if($recipe["shaped"]) {
             return new ShapedRecipe($recipe["shape"], $ingredients, $result);
         }
